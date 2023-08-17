e8b6cb0a7dd57f88a333a75cdadb3768db89452d
Aleksander Sułkowski <63408297+oleksulkowski@users.noreply.github.com>
2023-08-17 20:44:39 +0100

    Fixed bugs, added tests
    Bug with packaging signal protection.

--- docs/app/serviceWorker.js
+++ docs/app/serviceWorker.js
@@ -1,5 +1,5 @@
 const appName = 'SynRecoder'
-const appCacheName = 'SynRecoder-6821dbdb1dcf47298230f6f81bf07f95';
+const appCacheName = 'SynRecoder-2562e82f977b41b0b016103339a807be';
 
 const preCacheFiles = ['images/favicon.ico', 'images/icon-vector.svg', 'images/icon-32x32.png', 'images/icon-192x192.png', 'images/icon-512x512.png', 'images/apple-touch-icon.png', 'images/index_background.png'];
 
